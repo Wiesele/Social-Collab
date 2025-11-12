a9d7c5a955489a673ecd1c3a472b41fb835bab0e
Marvin Rothmann <m.rothmann@fburi.den>
2025-11-12 18:52:13 +0100

    d

---
+++ MRO.SKM.GitProvider/GitProvider.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using MRO.SKM.SDK.Interfaces;
+
+namespace MRO.SKM.GitProvider;
+
+public class GitProvider: IProviderConfiguration
+{
+    public void RegisterServices(IServiceCollection services)
+    {
+        services.AddSingleton<GitNoAuthProviderServiceService>();
+        services.AddSingleton<ISourceProviderService>(x => x.GetRequiredService<GitNoAuthProviderServiceService>());
+    }
+}
