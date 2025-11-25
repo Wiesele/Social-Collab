317f8896d14ff02af45c075143ddceaf628dbb8a
Marvin Rothmann <m.rothmann@fburi.den>
2025-11-25 22:11:14 +0100

    monaco autodetect language

--- MRO.SKM.Docu/Components/Pages/Repository2/Code/Home.razor.cs
+++ MRO.SKM.Docu/Components/Pages/Repository2/Code/Home.razor.cs
@@ -1,28 +1,28 @@
-﻿using BlazorMonaco.Editor;
+﻿using System.Web;
+using BlazorMonaco.Editor;
 using BlazorMonaco.Languages;
 using BlazorMonaco;
-
 using Microsoft.AspNetCore.Components;
 using Microsoft.JSInterop;
 using MRO.SKM.Docu.Components.Layout;
 using MRO.SKM.Docu.Models;
 using MRO.SMK.Docu.ApplicationCore.Services;
 using MRO.SMK.SDK.Models;
+
 namespace MRO.SKM.Docu.Components.Pages.Repository2.Code;
 
 public partial class Home : ComponentBase
 {
     private RepositoryService RepositoryService { get; set; }
     private IJSRuntime JSRuntime { get; set; }
-    
-    [Parameter]
-    public string Id { get; set; }
-    
+
+    [Parameter] public string Id { get; set; }
+
     private Repository Repository { get; set; }
     private FileListItem SelectedFile { get; set; }
     private string SelectedFileContent { get; set; }
     private StandaloneCodeEditor? Editor;
-    
+
     public Home(RepositoryService repositoryService, IJSRuntime jsRuntime)
     {
         this.JSRuntime = jsRuntime;
@@ -34,22 +34,20 @@ public partial class Home : ComponentBase
         this.Repository = this.RepositoryService.GetById(this.Id);
         base.OnInitialized();
     }
-    
+
     private async Task FileSelected(FileListItem obj)
     {
         this.SelectedFile = obj;
         this.SelectedFileContent = await File.ReadAllTextAsync(this.SelectedFile.FullPath);
         await this.SetEditorValue();
     }
-    
+
     private StandaloneEditorConstructionOptions EditorConstructionOptions(StandaloneCodeEditor editor)
     {
         return new StandaloneEditorConstructionOptions
         {
             AutomaticLayout = true,
             ReadOnly = true,
-            Language = "c#",
-            Value = ""
         };
     }
 
@@ -59,11 +57,19 @@ public partial class Home : ComponentBase
         if (this.Editor != null)
         {
             var model = await this.Editor.GetModel();
-            var languageId = "csharp";
-            
-            await BlazorMonaco.Editor.Global.SetModelLanguage(this.JSRuntime, model, languageId);
-            
-            await this.Editor.SetValue(this.SelectedFileContent);
+
+            var models = await BlazorMonaco.Editor.Global.GetModels(model.JsRuntime);
+            var pathEncoded = this.SelectedFile.FullPath;
+            TextModel? newModel = models.FirstOrDefault(e => HttpUtility.UrlDecode(e.Uri) == pathEncoded);
+            if (newModel == null)
+            {
+                newModel = await BlazorMonaco.Editor.Global.CreateModel(model.JsRuntime,
+                    this.SelectedFileContent,
+                    null,
+                    pathEncoded);
+            }
+
+            await this.Editor.SetModel(newModel);
         }
     }
 }
