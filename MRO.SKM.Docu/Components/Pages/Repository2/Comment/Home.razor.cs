0d52a258c19d2b641ad245aa8d66348d15543549
Marvin Rothmann <m.rothmann@funkholding.de>
2025-12-01 15:40:23 +0100

    tmp

--- MRO.SKM.Docu/Components/Pages/Repository2/Comment/Home.razor.cs
+++ MRO.SKM.Docu/Components/Pages/Repository2/Comment/Home.razor.cs
@@ -7,8 +7,16 @@ namespace MRO.SKM.Docu.Components.Pages.Repository2.Comment;
 
 public partial class Home : BaseRepoPage
 {
+    private FileListItem SelectedFile { get; set; }
+    
     public Home(RepositoryService repositoryService) : base(repositoryService)
     {
         
+    }
+    
+    
+    private async Task FileSelected(FileListItem obj)
+    {
+        this.SelectedFile = obj;
     }
 }
