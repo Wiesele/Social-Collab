9ed3d9d01d8a1f3f60f94c9379c01ae178f80e37
Marvin Rothmann <m.rothmann@funkholding.de>
2025-12-11 23:53:41 +0100

    update gemini service

--- MRO.SKM.Google.Gemini/GeminiBaseService.cs
+++ MRO.SKM.Google.Gemini/GeminiBaseService.cs
@@ -12,6 +12,14 @@ public abstract class GeminiBaseService
     protected async Task<string> GenerateSimpleContent(string model, string apiKey, string prompt, string schema)
     {
         var client = new Client(apiKey: apiKey, vertexAI: false);
+        
+        var contents = new Content();
+        contents.Role = "user";
+        contents.Parts = new();
+        contents.Parts.Add(new()
+        {
+            Text = prompt
+        });
 
         var thinkConfig = new ThinkingConfig();
         thinkConfig.ThinkingBudget = 0;
@@ -21,39 +29,38 @@ public abstract class GeminiBaseService
         config.ResponseSchema = Schema.FromJson(schema);
         config.ThinkingConfig = thinkConfig;
 
-
-        // var response = await client.Models.GenerateContentAsync(
-        //     model: model,
-        //     contents: prompt,
-        //     config: config
-        // );
-        //
-        // var responseContent = response.Candidates[0].Content.Parts[0].Text;
-        //
-        // return this.CleanupString(responseContent);
-
-        var contents = new Content();
-        contents.Role = "user";
-        contents.Parts = new();
-        contents.Parts.Add(new()
-        {
-            Text = prompt
-        });
-
-        var response = client.Models.GenerateContentStreamAsync(
+        var response = await client.Models.GenerateContentAsync(
             model: model,
             contents: contents,
             config: config
         );
-
-        var result = "";
-
-        await foreach (var chunk in response)
-        {
-            result += chunk.Candidates.First().Content.Parts.First().Text;
-        }
-
-        return result;
+        
+        var responseContent = response.Candidates[0].Content.Parts[0].Text;
+        
+        return this.CleanupString(responseContent);
+        //
+        // var contents = new Content();
+        // contents.Role = "user";
+        // contents.Parts = new();
+        // contents.Parts.Add(new()
+        // {
+        //     Text = prompt
+        // });
+        //
+        // var response = client.Models.GenerateContentStreamAsync(
+        //     model: model,
+        //     contents: contents,
+        //     config: config
+        // );
+        //
+        // var result = "";
+        //
+        // await foreach (var chunk in response)
+        // {
+        //     result += chunk.Candidates.First().Content.Parts.First().Text;
+        // }
+        //
+        // return result;
     }
 
     private string CleanupString(string str)
